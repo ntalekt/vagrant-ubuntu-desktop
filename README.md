044944aa241128b878c6975366e1dcb0971edd3a
ntalekt <ntalekt@users.noreply.github.com>
2020-08-12 09:06:44 -0700

    bosh cli

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ A simple Vagrantfile to setup Ubuntu desktop environment with Google Chrome and 
 * Ops-Manager CLI - https://github.com/pivotal-cf/om
 * TKGI CLI - https://docs.pivotal.io/tkgi/1-8/installing-cli.html
 * kubectl-cli - https://docs.pivotal.io/tkgi/1-8/installing-kubectl-cli.html
+* bosh CLI - https://bosh.io/docs/cli-v2-install
 
 ### Usage
 -   Requires - <https://github.com/josenk/vagrant-vmware-esxi>
--- scripts/tgki_binaries.sh
+++ scripts/tgki_binaries.sh
@@ -4,6 +4,9 @@
 # https://docs.pivotal.io/tkgi/1-8/installing-cli.html
 # https://docs.pivotal.io/tkgi/1-8/installing-kubectl-cli.html
 echo -e "\e[33m**********\e[39mBegin TKGI & kubectl CLI install\e[33m**********\e[39m"
+#
+#
+echo -e "\e[33m**********\e[39mBegin tkgi, bosh, om & kubectl CLI install\e[33m**********\e[39m"
 if [ -d /vagrant/bin ]
 then
     # om CLI
@@ -13,6 +16,12 @@ then
     chmod +x om
     mv /home/vagrant/om /usr/local/bin/om
     # tkgi CLI
+
+    # bosh CLI - https://bosh.io/docs/cli-v2-install/
+    BOSHVERSION=$(curl -s https://github.com/cloudfoundry/bosh-cli/releases/latest/download 2>&1 | grep -Po [0-9]+\.[0-9]+\.[0-9]+)
+    curl -s https://api.github.com/repos/cloudfoundry/bosh-cli/releases/latest | grep browser_download_url | grep linux | cut -d '"' -f 4 | wget -qi -
+    chmod +x bosh-cli-${BOSHVERSION}-linux-amd64
+    mv /home/vagrant/bosh-cli-${BOSHVERSION}-linux-amd64 /usr/local/bin/bosh
     chmod +x /vagrant/bin/tkgi-linux-amd64-1.8.0-build.75
     mv /vagrant/bin/tkgi-linux-amd64-1.8.0-build.75 /usr/local/bin/tkgi
     # kubectl CLI
@@ -21,4 +30,4 @@ then
 else
     echo "Cannot find binaries"
 fi
-echo -e "\e[33m**********\e[39mEnd TKGI & kubectl CLI install\e[33m**********\e[39m"
+echo -e "\e[33m**********\e[39mEnd tkgi, bosh, om & kubectl CLI install\e[33m**********\e[39m"
